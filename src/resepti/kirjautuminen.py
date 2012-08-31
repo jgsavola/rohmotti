eda8d8590d8bbf89813f7ff7847bdf0424033edf
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-08-31 13:13:37 +0300

    Luo kunnollinen eväste kirjautumisessa
    Tee salakirjoitettu eväste, jossa on henkilön id, istunnon alkamisen
    aikaleima ja käyttäjän IP-osoite.

--- src/resepti/kirjautuminen.py
+++ src/resepti/kirjautuminen.py
@@ -7,15 +7,23 @@ import re
 import psycopg2
 import json
 import cgi
+import base64
 import Cookie
+import datetime
+import time
+import math
 
 from Henkilo import Henkilo
 import salasana
+from salaus import Salaus
 
 class Handler:
     def __init__(self, form, conf):
         self.form = form
         self._conf = conf
+
+        self.secret_key = '2M\x93\x9a \x9d\x86zV\x04\xf3?\x08\x8a\xba7'
+        self.salaus = Salaus(self.secret_key)
 
     @property
     def conf(self):
@@ -34,10 +42,14 @@ class Handler:
             C = Cookie.SimpleCookie()
             C.load(os.environ.get('HTTP_COOKIE', ''))
             try:
-                henkilo_id = C["henkilo_id"]
-                parameters = { 'status': '<p class="status">Tuttu henkilö %d</p>' % (int(henkilo_id.value)) }
+                encrypted_cookie_text = C['rohmotti']
             except KeyError:
                 parameters = { 'status': '<p class="status">KeyError</p>' }
+            else:
+                cookie_text = self.salaus.decrypt(base64.b64decode(encrypted_cookie_text.value))
+
+                parameters = { 'status': '<p class="status">Tuttu henkilö %s</p>' % (cookie_text) }
+
         elif os.environ['REQUEST_METHOD'] == 'POST':
             tunnus_input = self.form.getvalue("tunnus")
             salasana_input = self.form.getvalue("salasana")
@@ -56,8 +68,15 @@ class Handler:
                 salasana_hash = salasana.hash_password(salasana_input, suola)
                 if salasana_hash == henkilo.salasana:
                     parameters = { 'status': '<p class="status">Tervetuloa, henkilö %d!</p>' % (henkilo.henkilo_id) }
+
+                    timestamp = math.floor(time.time())
+
+                    today = datetime.datetime.today()
+
+                    cookie_text = "id=%d ip=%s time=%d rohmotti" % (henkilo.henkilo_id, os.environ.get('REMOTE_ADDR'), timestamp)
+
                     C = Cookie.SimpleCookie()
-                    C["henkilo_id"] = henkilo.henkilo_id
+                    C["rohmotti"] = base64.b64encode(self.salaus.encrypt(cookie_text))
 
                     headers.append(C.output())
 

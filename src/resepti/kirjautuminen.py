ed1233b40a5ae58682e02ef85f8ad71454f0362a
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-08-31 17:44:15 +0300

    Uloskirjautuminen POST-metodilla
    Järjestele uudelleen kirjautuminen-handleria.

--- src/resepti/kirjautuminen.py
+++ src/resepti/kirjautuminen.py
@@ -32,44 +32,68 @@ class Handler:
         return self._conf
 
     def render(self):
-        path_info = os.environ.get('PATH_INFO', '')
+        self.headers = []
+        self.headers.append('Content-Type: text/html; charset=UTF-8')
 
-        headers = []
-        headers.append('Content-Type: text/html; charset=UTF-8')
+        self.parameters = {}
 
-        parameters = {}
+        C = Cookie.SimpleCookie()
+        C.load(os.environ.get('HTTP_COOKIE', ''))
+        self.sessio = Sessio.new_from_cookie(C)
 
         if os.environ['REQUEST_METHOD'] == 'GET':
-            C = Cookie.SimpleCookie()
-            C.load(os.environ.get('HTTP_COOKIE', ''))
-            sessio = Sessio.new_from_cookie(C)
-
-            if sessio is not None:
-                if self.form.getvalue('logout') is not None:
-                    C['rohmotti']['expires'] = 0
-                    parameters = { 'status': '<p class="status">Uloskirjautuminen tehty.</p>' }
-                    headers.append(C.output())
-
-                    return [ headers, parameters ]
-
-                #
-                # Tarkista, että sessio on validi.
-                #
-                henkilo = Henkilo.load_from_database(sessio.henkilo_id)
-
-                if sessio.remote_addr != self.conf['effective_remote_addr']:
-                    parameters = { 'status': '<p class="status">Session ip-osoite ei täsmää käyttäjän ip-osoitteen kanssa!</p><pre>%s</pre>' % (str(sessio)) }
-                elif henkilo is None:
-                    parameters = { 'status': '<p class="status">Session käyttäjää %d ei löydy tietokannasta!</p>' % (sessio.henkilo_id) }
-                else:
-                    parameters = { 'status': '<p class="status">Tuttu henkilö %s</p>' % (sessio) }
+            self.handle_get()
         elif os.environ['REQUEST_METHOD'] == 'POST':
+            self.handle_post()
+
+        return [ self.headers, self.parameters ]
+
+    def handle_get(self):
+        if self.sessio is None:
+            self.parameters = { 'status': '<p class="status">Käyttäjä ei ole kirjautunut.</p>' }
+            return True
+
+        #
+        # Tarkista, että sessio on validi.
+        #
+        henkilo = Henkilo.load_from_database(self.sessio.henkilo_id)
+
+        if self.sessio.remote_addr != self.conf['effective_remote_addr']:
+            self.parameters = { 'status': '<p class="status">Session ip-osoite ei täsmää käyttäjän ip-osoitteen kanssa!</p><pre>%s</pre>' % (cgi.escape(str(self.sessio))) }
+        elif henkilo is None:
+            self.parameters = { 'status': '<p class="status">Session käyttäjää %d ei löydy tietokannasta!</p>' % (self.sessio.henkilo_id) }
+        else:
+            self.parameters = { 'status': '<p class="status">Tuttu henkilö %s</p>' % (self.sessio) }
+
+        return True
+
+    def handle_post(self):
+        action_input = self.form.getvalue("action")
+        if action_input is None or (action_input != 'login' and action_input != 'logout'):
+            self.parameters = { 'status': '<p class="status">Virheellinen toiminto.</p>' }
+            return True
+
+        if action_input == 'logout':
+            if self.sessio is None:
+                #
+                # Istuntoa ei ole, ei tehdä mitään.
+                #
+                self.parameters = { 'status': '<p class="status">OK</p>' }
+                return True
+
+            C = self.sessio.delete_cookie()
+            self.headers.append(C.output())
+            self.parameters = { 'status': '<p class="status">Uloskirjautuminen tehty.</p>' }
+
+            return True
+        elif action_input == 'login':
             tunnus_input = self.form.getvalue("tunnus")
             salasana_input = self.form.getvalue("salasana")
 
             if tunnus_input is None or salasana_input is None:
-                parameters = { 'status': '<p class="status">Anna tunnus ja salasana!</p>' }
-                return [ headers, parameters ]
+                self.parameters = { 'status': '<p class="status">Anna tunnus ja salasana!</p>' }
+
+                return True
 
             henkilo = Henkilo.load_from_database(tunnus=tunnus_input)
             if henkilo is not None:
@@ -80,18 +104,18 @@ class Handler:
                 suola = salasana.get_salt_from_hash(henkilo.salasana)
                 salasana_hash = salasana.hash_password(salasana_input, suola)
                 if salasana_hash == henkilo.salasana:
-                    parameters = { 'status': '<p class="status">Tervetuloa, henkilö %d!</p>' % (henkilo.henkilo_id) }
+                    self.parameters = { 'status': '<p class="status">Tervetuloa, henkilö %d!</p>' % (henkilo.henkilo_id) }
 
                     timestamp = int(math.floor(time.time()))
 
-                    sessio = Sessio(henkilo_id=henkilo.henkilo_id, start_timestamp=timestamp, remote_addr=self.conf['effective_remote_addr'])
-                    headers.append(sessio.create_cookie().output())
+                    self.sessio = Sessio(henkilo_id=henkilo.henkilo_id, start_timestamp=timestamp, remote_addr=self.conf['effective_remote_addr'])
+                    self.headers.append(self.sessio.create_cookie().output())
 
                     #
                     # Kaikki OK!
                     #
-                    return [ headers, parameters ]
+                    return True
 
-            parameters = { 'status': '<p class="status">Kirjautumisyritys hyvä, mutta ei riittävä!</p>' }
+            self.parameters = { 'status': '<p class="status">Kirjautumisyritys hyvä, mutta ei riittävä!</p>' }
 
-        return [ headers, parameters ]
+        return True

cfeb42b9746d08efee236f35dd2f4e3fe628c649
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-09-01 00:32:12 +0300

    Lisää testiskripti kommentille

---
+++ src/resepti/test_kommentti.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+import pwd
+import psycopg2
+from DatabaseObject import DatabaseObject
+from Kommentti import Kommentti
+
+dbuser = pwd.getpwuid(os.getuid()).pw_name
+dbname = dbuser
+conn = psycopg2.connect("dbname=%s user=%s" % (dbname, dbuser))
+
+DatabaseObject.setDatabaseConnection(conn)
+
+kuva = Kommentti.load_from_database(3)
+
+print "kommentti: %d -- %s" % (kuva.kommentti_id, kuva.teksti)

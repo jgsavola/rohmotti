b34fa36a8ad198a5bf88924db9a4f35a5a6171f5
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-08-31 13:15:59 +0300

    Luo sessio-moduuli istunnon evästeiden hallintaan

---
+++ src/resepti/sessio.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import base64
+import Cookie
+import math
+
+from salaus import Salaus
+
+class Sessio:
+    SECRET_KEY = '2M\x93\x9a \x9d\x86zV\x04\xf3?\x08\x8a\xba7'
+    salaus = Salaus(SECRET_KEY)
+
+    def __init__(self, henkilo_id, start_timestamp, remote_addr):
+        self.henkilo_id = henkilo_id
+        self.start_timestamp = start_timestamp
+        self.remote_addr = remote_addr
+
+    @property
+    def conf(self):
+        """Get the configuration"""
+        return self._conf
+
+    def create_cookie(self):
+        cookie_plaintext = str(self)
+
+        C = Cookie.SimpleCookie()
+        C["rohmotti"] = base64.b64encode(self.salaus.encrypt(cookie_plaintext))
+
+        return C
+
+    @classmethod
+    def new_from_cookie(cls, C):
+        try:
+            encrypted_cookie_text = C['rohmotti']
+        except KeyError:
+            return
+
+        cookie_text = cls.salaus.decrypt(base64.b64decode(encrypted_cookie_text.value))
+        cookie_params = cookie_text.split(' ')
+
+        cookie_dict = {}
+        for param in cookie_params:
+            kv = param.partition('=')
+            cookie_dict[kv[0]] = kv[2]
+
+        henkilo_id = int(cookie_dict['id'])
+        start_timestamp = int(cookie_dict['start'])
+        remote_addr = cookie_dict['ip']
+
+        rohmotti = cookie_dict['rohmotti']
+
+        return cls(henkilo_id=henkilo_id, start_timestamp=start_timestamp, remote_addr=remote_addr)
+
+    def __str__(self):
+        return "id=%d ip=%s start=%d rohmotti" % (self.henkilo_id, self.remote_addr, self.start_timestamp)
+
+
+if __name__ == "__main__":
+    from sessio import Sessio
+    s1 = Sessio(henkilo_id=999, start_timestamp=14135434, remote_addr='131.123.233.123')
+
+    cookie = s1.create_cookie()
+    print cookie
+
+    s2 = Sessio.new_from_cookie(cookie)
+    print s2

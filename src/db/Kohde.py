e32b4ac42770a80ace19591729a658cacf7ddf2a
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-09-05 01:37:49 +0300

    Toteuta loput metodit Kohde-luokasta
    Tavoite on, että Kohde-luokasta perivät luokat tekevät
    tietokantaoperaatiot atomisesti sekä päätauluunsa että kohde-tauluun.

--- src/db/Kohde.py
+++ src/db/Kohde.py
@@ -7,6 +7,129 @@ from Kommentti import Kommentti
 from SimpleDatabaseObject import SimpleDatabaseObject
 
 class Kohde(SimpleDatabaseObject):
+    table_name = 'reseptiohjelma.kohde'
+    id_column = 'kohde_id'
+    other_columns = ['luotu', 'omistaja']
+
+    @classmethod
+    def load_from_database(cls, _id, **kwargs):
+        select_columns = Kohde.other_columns
+
+        query = ("SELECT %s FROM %s WHERE %s = %%s" %
+                 (', '.join(select_columns),
+                  Kohde.table_name,
+                  Kohde.id_column))
+
+        my_cursor = _cursor = kwargs.get('_cursor', None)
+        row = None
+        super_object = None
+        try:
+            if my_cursor is None:
+                my_cursor = cls.conn.cursor()
+
+            super_object = super(Kohde, cls).load_from_database(_id, _cursor=my_cursor)
+
+            my_cursor.execute(query, (int(_id),))
+            row = my_cursor.fetchone()
+        except:
+            if _cursor is None:
+                cls.conn.rollback()
+            raise
+        else:
+            if _cursor is None:
+                cls.conn.commit()
+
+        for i, k in enumerate(select_columns):
+            setattr(super_object, k, row[i])
+
+        return super_object
+
+    @classmethod
+    def new(cls, **kwargs):
+        select_columns = Kohde.other_columns
+        select_columns_string = ', '.join(select_columns)
+
+        update_columns = sorted(set(Kohde.other_columns) & set(kwargs.keys()))
+        update_columns_string = ', '.join(map(lambda col: "%s = %%s" % (col), update_columns))
+        update_values = map(lambda k: kwargs[k], update_columns)
+
+        update_sql = ("UPDATE %s SET %s WHERE %s = %%s RETURNING %s" %
+                      (Kohde.table_name,
+                       update_columns_string,
+                       Kohde.id_column,
+                       select_columns_string))
+
+        my_cursor = _cursor = kwargs.get('_cursor', None)
+
+        row = None
+        try:
+            if my_cursor is None:
+                my_cursor = cls.conn.cursor()
+
+            #
+            # Kohde-taulu täytetään päätaulun DEFAULT-arvona
+            # kutsuttavassa PL/PgSQL-funktiossa hae_kohde(), joten
+            # super-metodia on kutsuttava ensin.
+            #
+            # Tällaista taulurakennetta voi pitää suunnitteluvirheenä,
+            # mutta käypähän malliksi monimutkaisesta
+            # tietokantaolioabstraktiosta.
+            #
+            kwargs['_cursor'] = my_cursor
+            super_object = super(Kohde, cls).new(**kwargs)
+
+            update_values.append(getattr(super_object, super_object.id_column))
+
+            my_cursor.execute(update_sql, update_values)
+
+            row = my_cursor.fetchone()
+        except:
+            if _cursor is None:
+                cls.conn.rollback()
+            raise
+        else:
+            if _cursor is None:
+                cls.conn.commit()
+
+        for i, k in enumerate(select_columns):
+            setattr(super_object, k, row[i])
+
+        return super_object
+
+    def save(self, **kwargs):
+        """Tallenna (päivitä) olion tiedot tietokantaan."""
+
+        update_columns = Kohde.other_columns
+        update_columns_string = ', '.join(map(lambda col: "%s = %%s" % (col), update_columns))
+        update_sql = ("UPDATE %s SET %s WHERE %s = %%s" %
+                      (Kohde.table_name,
+                       update_columns_string,
+                       Kohde.id_column))
+
+        update_values = map(lambda name: getattr(self, name), update_columns)
+        update_values.append(getattr(self, self.__class__.id_column))
+
+        my_cursor = _cursor = kwargs.get('_cursor', None)
+        try:
+            if my_cursor is None:
+                my_cursor = self.__class__.conn.cursor()
+
+            #
+            # Pitääkö "kohde"-taulu päivittää ennen vai jälkeen
+            # päätaulun päivittämisen?
+            #
+            kwargs['_cursor'] = my_cursor
+            super(Kohde, self.__class__).save(self, **kwargs)
+
+            my_cursor.execute(update_sql, update_values)
+        except:
+            if _cursor is None:
+                self.__class__.conn.rollback()
+            raise
+        else:
+            if _cursor is None:
+                self.__class__.conn.commit()
+
     @classmethod
     def delete(cls, _id, **kwargs):
         delete_sql = 'DELETE FROM reseptiohjelma.kohde WHERE kohde_id = %s'
--- src/db/test_resepti2.py
+++ src/db/test_resepti2.py
@@ -14,8 +14,8 @@ conn = psycopg2.connect("dbname=%s user=%s" % (dbname, dbuser))
 
 DatabaseObject.setDatabaseConnection(conn)
 
-for id in Resepti2.load_ids():
-    o = Resepti2.load_from_database(id)
+for id in Resepti3.load_ids():
+    o = Resepti3.load_from_database(id)
     print ": %d -- %s" % (o.resepti_id, o.nimi)
 
 for id in Kommentti.load_ids():
@@ -32,15 +32,22 @@ for id in Kommentti.load_ids():
 # print "delete"
 # Resepti2.delete(r.resepti_id)
 
+print "Korinttikeksit"
+k = Resepti3.load_from_database(68)
+print "    %d %s %s %s" % (k.resepti_id, k.nimi, k.omistaja, k.luotu)
 
 print "Resepti3"
 print "new"
-r = Resepti3.new(nimi='testi_' + str(time.time()), valmistusohje='valmista')
-print "    %d %s" % (r.resepti_id, r.nimi)
+r = Resepti3.new(nimi='testi_' + str(time.time()), valmistusohje='valmista', omistaja=67)
+r2 = Resepti3.new(nimi='testi2_' + str(time.time()), valmistusohje='valmista', omistaja=67)
+print "    %d %s %s %s" % (r.resepti_id, r.nimi, r.omistaja, r.luotu)
+print "    %d %s %s %s" % (r2.resepti_id, r2.nimi, r2.omistaja, r2.luotu)
 
 print "update"
 r.valmistusohje = r.valmistusohje + "_update"
+r.omistaja = 64
 r.save()
 
-print "delete"
-Resepti3.delete(r.resepti_id)
+#print "delete"
+#Resepti3.delete(r.resepti_id)
+
